57e7445db937b66f154d9c544bc19a3a60c376c0
Rimi Chakraborty <143891192+rimichak@users.noreply.github.com>
2025-07-08 08:25:35 +0000

    added code

---
+++ main.py
@@ -0,0 +1,59 @@
+def show_menu():
+
+    print("1.Add Expenses")
+    print("2.View Total")
+    print("3.View Expenses by category")
+    print("4.Exit")
+
+def get_user_choice():
+
+    choice = input("Enter your choice from 1 to 4:")
+    return choice
+
+def add_expense():
+    amount = float(input("Enter amount:"))
+    category = input("Enter category:")
+    description = input("Enter a short description:")
+    
+    return{"amount":amount, "category":category, "description":description}
+
+def save_expense(expense, expenses_list):
+    expenses_list.append(expense)
+    return(expenses_list)
+
+def calculate_total(expenses_list)
+    total = 0
+    for expense in expenses_list:
+        total += expense
+        return(total)
+    
+def show_total(total):
+
+    print(f"Show the total:{total:.2f}")
+
+def get_expenses_by_category(expenses_list, category):
+    pass
+
+def show_expenses(expenses_list):
+    pass
+
+def get_all_categories(expenses_list):
+    pass
+
+def show_category_report(expenses_list):
+    pass
+
+def exit_program():
+    pass
+
+def handle_choice(choice, expenses_list):
+    pass
+
+def main():
+    pass
+
+def valid_choices(choice):
+    pass
+
+def get_category_input():
+    pass
